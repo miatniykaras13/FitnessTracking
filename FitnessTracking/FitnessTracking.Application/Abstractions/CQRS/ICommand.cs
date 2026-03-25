d0e3141e2ffbc15efeeb880e9c8662abbae38494
Vlad Laptop <vladg13012007@gmail.com>
2026-03-25 10:37:26 +0300

    refactor: add cqrs abstractions

---
+++ FitnessTracking/FitnessTracking.Application/Abstractions/CQRS/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace FitnessTracking.Application.Abstractions.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
---
+++ FitnessTracking/FitnessTracking.Application/Abstractions/CQRS/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace FitnessTracking.Application.Abstractions.CQRS;
+
+public interface IQuery<out TResponse> : IRequest<TResponse>
+    where TResponse : notnull
+{
+}
