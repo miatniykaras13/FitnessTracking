c215ecbf360abfa30238b3153f8aa06edad33f29
Vlad Laptop <vladg13012007@gmail.com>
2026-03-26 10:47:25 +0300

    fix: fix validation rules for merge patch dtos

--- FitnessTracking/FitnessTracking.Application/Validators/MergePatchWorkoutDtoValidator.cs
+++ FitnessTracking/FitnessTracking.Application/Validators/MergePatchWorkoutDtoValidator.cs
@@ -8,19 +8,30 @@ public class MergePatchWorkoutDtoValidator : AbstractValidator<MergePatchWorkout
 {
     public MergePatchWorkoutDtoValidator()
     {
-        RuleFor(x => x.Title).NotEmpty();
-        RuleFor(x => x.Type)
-            .NotEmpty()
-            .Must(BeValidWorkoutType);
-        RuleFor(x => x.Duration)
-            .GreaterThan(TimeSpan.Zero);
-        RuleFor(x => x.CaloriesBurned)
-            .GreaterThanOrEqualTo(0);
-        RuleFor(x => x.WorkoutDate)
-            .NotEqual(default(DateTime));
+        When(x => x.Title is not null, () => { RuleFor(x => x.Title).NotEmpty(); });
+        When(x => x.Type is not null, () =>
+        {
+            RuleFor(x => x.Type)
+                .NotEmpty()
+                .Must(BeValidWorkoutType);
+        });
+        When(x => x.Duration is not null, () =>
+        {
+            RuleFor(x => x.Duration)
+                .GreaterThan(TimeSpan.Zero);
+        });
+        When(x => x.CaloriesBurned is not null, () =>
+        {
+            RuleFor(x => x.CaloriesBurned)
+                .GreaterThanOrEqualTo(0);
+        });
+        When(x => x.WorkoutDate is not null, () =>
+        {
+            RuleFor(x => x.WorkoutDate)
+                .NotEqual(default(DateTime));
+        });
     }
 
     private static bool BeValidWorkoutType(string? type) =>
         !string.IsNullOrWhiteSpace(type) && Enum.TryParse<WorkoutType>(type, true, out _);
 }
-
