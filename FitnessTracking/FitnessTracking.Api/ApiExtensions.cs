5f2e284ac4b79a27bfe183508ed7c137b8960458
Vlad Laptop <vladg13012007@gmail.com>
2026-03-31 21:09:54 +0300

    refactor: add extension methods to reduce Program.cs overload

--- FitnessTracking/FitnessTracking.Api/ApiExtensions.cs
+++ FitnessTracking/FitnessTracking.Api/ApiExtensions.cs
@@ -1,17 +1,66 @@
 ﻿using FitnessTracking.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+using FitnessTracking.Api.Middleware;
+using Carter;
+using Microsoft.Extensions.FileProviders;
 
 namespace FitnessTracking.Api;
 
 public static class ApiExtensions
 {
-    public static async Task<WebApplication> UseMigrating(this WebApplication app)
+    public static WebApplication UseApiPipeline(this WebApplication app, IConfiguration configuration)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseUploadsStaticFiles(configuration);
+        app.MapCarter();
+        app.UseDevelopmentSwagger();
+        app.UseHttpsRedirection();
+
+        return app;
+    }
+
+    public static async Task<WebApplication> UseMigratingAsync(this WebApplication app)
     {
         using (var scope = app.Services.CreateScope())
         {
             var dbContext = scope.ServiceProvider.GetRequiredService<FitnessTrackingDbContext>();
             await dbContext.Database.EnsureCreatedAsync();
         }
+        return app;
+    }
+
+    private static WebApplication UseUploadsStaticFiles(this WebApplication app, IConfiguration configuration)
+    {
+        var uploadsRoot = Path.Combine(
+            app.Environment.ContentRootPath,
+            configuration["FileStorage:RootPath"] ?? "uploads");
+
+        Directory.CreateDirectory(uploadsRoot);
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(uploadsRoot),
+            RequestPath = "/uploads"
+        });
+
+        return app;
+    }
+
+    private static WebApplication UseDevelopmentSwagger(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            return app;
+        }
+
+        app.UseSwagger();
+        app.UseSwaggerUI(o =>
+        {
+            o.SwaggerEndpoint("/swagger/v1/swagger.json", "FitnessTracking.Api v1");
+            o.RoutePrefix = "docs";
+        });
+
         return app;
     }
 }
